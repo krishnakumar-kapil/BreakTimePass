60d41bd7aed7a53e91302d127599a556bad8ece9
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2015-07-31 22:04:13 +0530

    Changed to new js file

--- timerStore.js
+++ timerStore.js
@@ -1,4 +1,4 @@
-var storage = chrome.storage.local; 
+// var storage = chrome.storage.local; 
 var startTime = new Date();
 
 var currentTab = function(){
@@ -13,24 +13,38 @@ var currentTab = function(){
 
   var getHostName = function(href) {
     var l = document.createElement("a");
-    l.href = href;
     return l;
   };
 
-  function saveChanges(hostName, startTime) {
+  function saveChanges(lin) {
+    var hostName = lin.href;
     var currentTime = new Date();
     var timeVal = currentTime - startTime;
-    if(!storage.get(hostName)){
-      storage.set({hostName: timeVal}, function() {
-        // Notify that we saved.
-        console.log(hostName+' added');
-      });
-    } else {
-      var time = storage.get(hostName);
-      storage.set({hostName: time + timeVal}, function(){
-        console.log('stored new time');
-      })
-    }
+    chrome.storage.local.get(hostName, function(result){
+      console.log(result + " : "+ result[hostName]);
+      var store = {};
+      store[hostName] = timeVal;
+      if(!result[hostName]){
+        chrome.storage.local.set(store);
+      } else {
+        store[hostName] = result[hostName] + timeVal;
+        chrome.storage.local.set(store);
+      }
+    });
+
+    console.log(hostName + ": "+chrome.storage.local);
+
+    // if(!storage.get(hostName)){
+    //   storage.set({hostName: timeVal}, function() {
+    //     // Notify that we saved.
+    //     console.log(hostName+' added');
+    //   });
+    // } else {
+    //   var time = storage.get(hostName);
+    //   storage.set({hostName: time + timeVal}, function(){
+    //     console.log('stored new time');
+    //   })
+    // }
 
      // // Save it using the Chrome extension storage API.
      // chrome.storage.sync.set({'value': theValue}, function() {
@@ -40,7 +54,7 @@ var currentTab = function(){
    }
 
    chrome.tabs.onActivated.addListener(function(){
-      saveChanges(currentTab(), currentTime);
+      saveChanges(currentTab());
       startTime = new Date();
    });
 
