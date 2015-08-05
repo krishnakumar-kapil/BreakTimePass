a3a1b14113160052be627c31adb622c789e62cb4
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2015-08-05 17:49:28 +0530

    Displaying chart

--- chartDraw.js
+++ chartDraw.js
@@ -1,36 +1,6 @@
-function loadDataChart(){
-  var data = {
-    labels: chrome.local.get(null,function(result){
-      return Object.keys(result);
-    }),
-    series: chrome.local.get(null,function(result){
-      return Object.value(result);
-    }),
-  };
-
-  var options = {
-    labelInterpolationFnc: function(value) {
-      return value[0]
-    }
-  };
-
-  var responsiveOptions = [
-    ['screen and (min-width: 640px)', {
-      chartPadding: 30,
-      labelOffset: 100,
-      labelDirection: 'explode',
-      labelInterpolationFnc: function(value) {
-        return value;
-      }
-    }],
-    ['screen and (min-width: 1024px)', {
-      labelOffset: 80,
-      chartPadding: 20
-    }]
-  ];
-
-  new Chartist.Pie('.ct-chart', data, options, responsiveOptions);
-}
+document.addEventListener('DOMContentLoaded', function () {
+  loadArray();
+});
 
 function loadArray(){
     var sortable = [];
@@ -39,7 +9,7 @@ function loadArray(){
 
       for (var url in result){
         if(url !== "expireDate"){
-          console.log(url);
+          // console.log(url);
           sortable.push([url, result[url]]);
         }
       }
@@ -51,28 +21,62 @@ function loadArray(){
       var max = 10 < sortable.length? 10: sortable.length;
       console.log(max);
       for (var i=0; i< max; i++) {
-          console.log(sortable[i]);
-          console.log(sortable[i][0]);
+          // console.log(sortable[i]);
+          // console.log(sortable[i][0]);
           html+='<li>'+(sortable[i])[0]+ ": "+convertMiliSecHours((sortable[i])[1])+'</li>';
       }
       html+='</ol>'
       document.getElementById('urlList').innerHTML+= html;
-      // display(sortable);
+      loadDataChart();
+    });
+}
+
+function loadDataChart(){
+  chrome.storage.local.get(null,function(result){
+      var keys = Object.keys(result);
+      console.log(keys);
+      var vals = [];
+      for(var url in result){
+        vals.push(result[url]);
+      }
+
+      var data = {
+        labels: keys,
+        series: vals
+      };
+
+      var options = {
+        labelInterpolationFnc: function(value) {
+          return value[0];
+        }
+      };
+
+      var responsiveOptions = [
+        ['screen and (min-width: 640px)', {
+          chartPadding: 30,
+          labelOffset: 100,
+          labelDirection: 'explode',
+          labelInterpolationFnc: function(value) {
+            return value;
+          }
+        }],
+        ['screen and (min-width: 1024px)', {
+          labelOffset: 80,
+          chartPadding: 20
+        }]
+      ];
+
+      console.log(data);
+      console.log(options);
+      console.log(responsiveOptions);
+      new Chartist.Pie('.ct-chart', data, options, responsiveOptions);
+
     });
 
-
-    // for (var url in storage) {
-    //   console.log(url +" urlVal: "+storage[url]);
-    //   var urlObj = {"name": url , "value": storage[url]};
-    //   urlArray.push(urlObj);
-    // }
-    // console.log(urlArray);
-    // // urlArray.splice(0,10);
-    // // Sort the URLs by the number of times the user typed them.
-    // urlArray.sort(function(a, b) {
-    //   return b.value - a.value;
-    // });
+  
 }
+
+
 
 function convertMiliSecHours(d){
   d = Number(d);
@@ -83,21 +87,18 @@ function convertMiliSecHours(d){
   return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
 }
 
-function display(urlArray){
-  var html='<ol>';
-  for (var i=0; i<=10; i++) {
-      console.log(urlArray[i]);
-      html+='<li>'+urlArray[i]+'</li>';
-  }
-  html+='</ol>'
-  document.getElementById('urlList').innerHTML+= html;
-}
+// function display(urlArray){
+//   var html='<ol>';
+//   for (var i=0; i<=10; i++) {
+//       console.log(urlArray[i]);
+//       html+='<li>'+urlArray[i]+'</li>';
+//   }
+//   html+='</ol>'
+//   document.getElementById('urlList').innerHTML+= html;
+// }
 
 chrome.storage.onChanged.addListener(function(){
   // loadDataChart();
   loadArray();
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  loadArray();
-});
